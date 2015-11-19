bec9e71a71e8158458af0cf07a310a4e72db0430
Paul Pearce <pearce@cs.berkeley.edu>
2015-11-18 19:35:26 -0500

    Changed dns schema

--- ztag/transforms/dns.py
+++ ztag/transforms/dns.py
@@ -15,17 +15,14 @@ class DNSTransform(ZMapTransform):
 
     def _transform_responses(self, responses):
         error = False
-
         ret = []
 
         for response in responses:
-
             if response["rdata_is_parsed"] == 1:
                 r = dict()
                 r["name"] = response["name"]
                 r["type"] = response["type_str"]
                 r["response"] = response["rdata"]
-
                 ret.append(r)
             else:
                 error = True
@@ -36,54 +33,38 @@ class DNSTransform(ZMapTransform):
         classification = obj['classification']
         if classification != "dns":
             raise errors.IgnoreObject(classification)
-        if int(obj['success']) != 1 or int(obj['app_success']) != 1:
+        if int(obj['success']) != 1:
             raise errors.IgnoreObject("Not a DNS resposne")
         zout = ZMapTransformOutput()
         out = dict()
-        out["support"] = True
-        out["questions"] = bool(len(obj["dns_questions"]))
-        out["answers"] = bool(len(obj["dns_answers"]))
-        out["authorities"] = bool(len(obj["dns_authorities"]))
-        out["additionals"] = bool(len(obj["dns_additionals"]))
+        out["speaks_dns"] = True
         errors_present = False
 
         if obj["dns_parse_err"] == True:
             errors_present = True
 
-        if out["questions"]:
+        out["questions"] = []
 
-            out["questions"] = []
-
-            for question in obj["dns_questions"]:
-                q = dict()
-                q["name"] = question["name"]
-                q["type"] = question["qtype_str"]
-
-                out["questions"].append(q)
+        for question in obj["dns_questions"]:
+            q = dict()
+            q["name"] = question["name"]
+            q["type"] = question["qtype_str"]
+            out["questions"].append(q)
 
         for field in ("answers", "authorities", "additionals"):
-
-            if out[field]:
-
-                (response_errors, responses) = \
-                    self._transform_responses(obj["dns_%s" % field])
-
-                if response_errors:
-                    errors_present = True
-
-                out[field] = responses
+            (response_errors, responses) = self._transform_responses(obj["dns_%s" % field])
+            if response_errors:
+                errors_present = True
+            out[field] = responses
 
         out["errors"] = errors_present
+	out["open_resolver"] = bool(len(out["answers"] + out["additionals"] + out["authorities"]))
+        out["resolves_correctly"] = False
 
-        answered_correctly = False
-
-        if out["answers"]:
-            for answer in out["answers"]:
-                if answer["response"] == CORRECT_RESPONSE:
-                    answered_correctly = True
-                    break
-
-        out["open_resolver"] = answered_correctly
+        for answer in out["answers"]:
+            if answer["type"] == "A" and answer["response"] == CORRECT_RESPONSE:
+                out["resolves_correctly"] = True
+                break
 
         zout.transformed = out
         return zout
