fff54a513869e342dadd81628a44eb9396e5d7ab
Zakir Durumeric <zakird@gmail.com>
2015-12-02 19:19:23 -0600

    indent

--- ztag/transforms/fox.py
+++ ztag/transforms/fox.py
@@ -35,44 +35,44 @@ class NiagaraFoxTransform(ZGrabTransform):
         auth_agent_type = fox['auth_agent_type'].resolve()
 
         out = dict()
-		if instance_number:
-			out["instance_number"] = instance_number
+        if instance_number:
+            out["instance_number"] = instance_number
         if version:
-			out["version"] = version
+            out["version"] = version
         if id_:
-			out["id"] = id_
-		if hostname:
-			out["hostname"] = hostname
-		if host_address:
-			out["host_address"] = host_address
-		if app_name:
-			out["app_name"] = app_name
-		if app_version:
-			out["app_version"] = app_version
-		if vm_name:
-			out["vm_name"] = vm_name
-		if vm_version:
-			out["vm_version"] = vm_version
-		if os_name:
-			out["os_name"] = os_name
-		if os_version:
-			out["os_version"] = os_version
-		if station_name:
-			out["station_name"] = station_name
-		if language:
-			out["language"] = language
-		if time_zone:
-			out["time_zone"] = time_zone
-		if host_id:
-			out["host_id"] = host_id
-		if vm_uuid:
-			out["vm_uuid"] = vm_uuid
-		if brand_id:
-			out["brand_id"] = brand_id
-		if sys_info:
-			out["sys_info"] = sys_info
-		if auth_agent_type:
-			out["auth_agent_type"] = auth_agent_type
+            out["id"] = id_
+        if hostname:
+            out["hostname"] = hostname
+        if host_address:
+            out["host_address"] = host_address
+        if app_name:
+            out["app_name"] = app_name
+        if app_version:
+            out["app_version"] = app_version
+        if vm_name:
+            out["vm_name"] = vm_name
+        if vm_version:
+            out["vm_version"] = vm_version
+        if os_name:
+            out["os_name"] = os_name
+        if os_version:
+            out["os_version"] = os_version
+        if station_name:
+            out["station_name"] = station_name
+        if language:
+            out["language"] = language
+        if time_zone:
+            out["time_zone"] = time_zone
+        if host_id:
+            out["host_id"] = host_id
+        if vm_uuid:
+            out["vm_uuid"] = vm_uuid
+        if brand_id:
+            out["brand_id"] = brand_id
+        if sys_info:
+            out["sys_info"] = sys_info
+        if auth_agent_type:
+            out["auth_agent_type"] = auth_agent_type
 
         if not bool(out):
             raise errors.IgnoreObject("Empty output dict")
