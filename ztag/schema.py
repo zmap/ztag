7fd4607745150d58976fb0d3c00c8e949e93f819
Zakir Durumeric <zakird@gmail.com>
2017-07-14 09:02:25 -0400

    Adding www queries to alexa schema (#73)
    * Adding www queries to alexa schema
    
    * removing accidental line break
    
    * Remove other accidental line break
    
    * removing exports

--- ztag/schema.py
+++ ztag/schema.py
@@ -1437,15 +1437,21 @@ website = Record({
                     "tls":ztag_tls,
                     "heartbleed":ztag_heartbleed,
                     "dhe": ztag_dh,
-                    "export_rsa": ztag_rsa_export,
-                    "export_dhe": ztag_dh_export,
+                    "export_rsa": ztag_rsa_export, # wrong name. should be rsa_export
+                    "export_dhe": ztag_dh_export,  # wrong name. should be dhe_export
                     "tls_1_1": ztag_tls_support,
                     "tls_1_2": ztag_tls_support,
                     "ecdhe": ztag_ecdh,
+                }),
+                "https_www":SubRecord({
+                    "tls":ztag_tls,
                 })
             }),
             Port(80):SubRecord({
                 "http":SubRecord({
+                    "get":ztag_http,
+                }),
+                "http_www":SubRecord({
                     "get":ztag_http,
                 }),
             }),
