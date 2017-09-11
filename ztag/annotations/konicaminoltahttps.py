47bbb2626429fe62385f90f02f47fd765f4d2711
Zakir Durumeric <zakird@gmail.com>
2017-09-11 14:08:11 -0400

    konica minolta works

--- ztag/annotations/konicaminoltahttps.py
+++ ztag/annotations/konicaminoltahttps.py
@@ -17,9 +17,11 @@ class KonicaMinoltaHTTPS(TLSTag):
     }
 
     def process(self, obj, meta):
-        org = obj["certificate"]["parsed"]["issuer"]["organization"][0]
-        if org == "Konica Minolta":
+        org = obj["certificate"]["parsed"]["issuer"]["organization"][0].lower()
+        if org == "konica minolta":
             meta.global_metadata.manufacturer = Manufacturer.KONICA_MINOLTA
+            meta.tags.add("embedded")
+            return meta
 
 
 class KonicaMinoltaBizHub(TLSTag):
@@ -40,8 +42,18 @@ class KonicaMinoltaBizHub(TLSTag):
     }
 
     def process(self, obj, meta):
-        org = obj["certificate"]["parsed"]["issuer"]["organization"][0]
-        if org == "Konica Minolta":
+        org = obj["certificate"]["parsed"]["issuer"]["organization"][0].lower()
+        if org == "konica minolta":
             meta.global_metadata.manufacturer = Manufacturer.KONICA_MINOLTA
-        return meta
+            for ou in obj["certificate"]["parsed"]["subject"]["organizational_unit"]:
+                if "bizhub" in ou:
+                    ou = ou.replace("KONICA MINOLTA", "").replace("bizhub", "").strip()
+                    if ou:
+                        product = " ".join(["Bizhub", ou])
+                        meta.global_metadata.product = product
+                    else:
+                        meta.global_metadata.product = "Bizhub"
+                    meta.global_metadata.device_type = Type.PRINTER
+                    meta.tags.add("embedded")
+                    return meta
 
