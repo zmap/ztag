341ad44f35e9f3ac4a43730373518ecb1c27f159
Zakir Durumeric <zakird@gmail.com>
2017-09-11 12:07:00 -0400

    working thecus

--- ztag/annotations/thecus.py
+++ ztag/annotations/thecus.py
@@ -11,17 +11,19 @@ class ThecusNAS(TLSTag):
         "global_metadata":{
           "manufacturer":Manufacturer.THECUS,
           "device_type":Type.NAS,
-          "product":"n7700",
+          "product":"N7700",
         },
         "tags":["embedded"]
       }
     }
 
     def process(self, obj,meta):
-        ou = obj["certificate"]["parsed"]["issuer"]["organizational_unit"][0]
-        if ou == "OpenCMC Group":
-            meta.global_metadata.manufacturer = Manufacturer.DELL
-            meta.global_metadata.device_type = Type.SERVER_MANAGEMENT
+        o = obj["certificate"]["parsed"]["subject"]["organization"][0]
+        ou = obj["certificate"]["parsed"]["subject"]["organizational_unit"][0]
+        if o == "Thecus Technology Corp.":
+            meta.global_metadata.manufacturer = Manufacturer.THECUS
+            meta.global_metadata.device_type = Type.NAS
+            meta.global_metadata.product = ou
             meta.tags.add("embedded")
             return meta
 
